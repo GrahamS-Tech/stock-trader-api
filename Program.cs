42568370442357850ea5eeb4f70ecd4a58d7c952
Graham Smith <graham.k.smith@live.com>
2023-10-11 13:27:01 -0700

    Azure testing

--- Program.cs
+++ Program.cs
@@ -36,15 +36,12 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
     };
 });
 
-var connString = builder.Configuration.GetConnectionString("azurePostgres");
+//var connString = builder.Configuration.GetConnectionString("azurePostgres");
 //System.Diagnostics.Debug.WriteLine(connString);
-EventLog eventlog = new EventLog();
-eventlog.Source = "ConnectionStringLog";
-eventlog.WriteEntry(connString, EventLogEntryType.Information);
 builder.Services.AddSingleton<ISessionFactory>((provider) => { 
 var cfg = new NHibernate.Cfg.Configuration();
     cfg.Configure(".\\Adapters\\Mappings\\hibernate.cfg.xml");
-    cfg.SetProperty("connection.connection_string", connString);
+    //cfg.SetProperty("connection.connection_string", connString);
     cfg.CurrentSessionContext<WebSessionContext>();
     return cfg.BuildSessionFactory();
 });
