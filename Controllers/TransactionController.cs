925f3c9f8d99c9d92fad344a240ddf04acbd52d7
Graham Smith <graham.k.smith@live.com>
2023-11-01 12:21:18 -0700

    Transaction controller bug fix

--- Controllers/TransactionController.cs
+++ Controllers/TransactionController.cs
@@ -44,7 +44,7 @@ public class TransactionController : ControllerBase
             response.Message = $"You only have {holdingDetails.Shares} share(s) available to sell";
         } else if (newTransaction.TransactionType == "Buy" && newTransaction.MarketValue * newTransaction.Shares < fiatDetails.Balance) {
             response.Status = "Error";
-            response.Message = "Insufficient funds to complete this purchase. Add funds from the My Account page.";
+            response.Message = "Insufficient funds to complete this purchase. Add funds from the My Account page. " + fiatDetails.Balance.ToString();
         }
         else {
             transaction new_transaction = new transaction();
