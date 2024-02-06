6a1902b62c9993eee17bb12515248c778a580ea4
Graham Smith <graham.k.smith@live.com>
2024-02-06 10:25:29 -0500

    Added endpoints for stock news

--- Controllers/StockDataController.cs
+++ Controllers/StockDataController.cs
@@ -1,11 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using StockTraderAPI.Adapters;
-using Microsoft.Extensions.Configuration;
 using System.Security.Claims;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Microsoft.Extensions.Options;
 
 namespace StockTraderAPI.Controllers;
 
@@ -181,6 +179,174 @@ public class StockDataController : ControllerBase
             response.Message = "Stock data retrieved successfully";
         }
         catch (Exception err) {
+            response.Status = "Error";
+            response.Message = "Could not retrieve stock data";
+        }
+
+        jsonResponse = JsonConvert.SerializeObject(response);
+        if (response.Status == "success") {
+            return Ok(jsonResponse);
+        }
+        else {
+            return BadRequest(jsonResponse);
+        }
+    }
+
+    [HttpGet("TopMovers")]
+    [Authorize]
+    public async Task<IActionResult> GetTopMovers()
+    {
+        var apiKey = Environment.GetEnvironmentVariable("AlphaVantageAPIKey");
+        var response = new api_response<object> { };
+        var jsonResponse = "";
+        var currentUser = ValidateUser();
+
+        if (currentUser.Id is null) {
+            response.Status = "error";
+            response.Message = "User account not found";
+            jsonResponse = JsonConvert.SerializeObject(response);
+            return NotFound(jsonResponse);
+        }
+
+        var url = $"https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey={apiKey}";
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+        try {
+            var client = _clientFactory.CreateClient("stockData");
+            var data = await client.SendAsync(request);
+            dynamic responseBody = await data.Content.ReadAsStringAsync();
+            JObject result = JsonConvert.DeserializeObject<JObject>(responseBody);
+            response.Data = result;
+            response.Status = "success";
+            response.Message = "Stock data retrieved successfully";
+        }
+        catch (Exception err) {
+            response.Status = "Error";
+            response.Message = "Could not retrieve stock data";
+        }
+
+        jsonResponse = JsonConvert.SerializeObject(response);
+        if (response.Status == "success") {
+            return Ok(jsonResponse);
+        }
+        else {
+            return BadRequest(jsonResponse);
+        }
+    }
+
+    [HttpGet("News")]
+    [Authorize]
+    public async Task<IActionResult> GetNews()
+    {
+        var apiKey = Environment.GetEnvironmentVariable("AlphaVantageAPIKey");
+        var response = new api_response<object> { };
+        var jsonResponse = "";
+        var currentUser = ValidateUser();
+
+        if (currentUser.Id is null) {
+            response.Status = "error";
+            response.Message = "User account not found";
+            jsonResponse = JsonConvert.SerializeObject(response);
+            return NotFound(jsonResponse);
+        }
+
+        var url = $"https://www.alphavantage.co/query?function=NEWS_SENTIMENT&apikey={apiKey}";
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+        try {
+            var client = _clientFactory.CreateClient("stockData");
+            var data = await client.SendAsync(request);
+            dynamic responseBody = await data.Content.ReadAsStringAsync();
+            JObject result = JsonConvert.DeserializeObject<JObject>(responseBody);
+            response.Data = result;
+            response.Status = "success";
+            response.Message = "Stock data retrieved successfully";
+        }
+        catch (Exception err) {
+            response.Status = "Error";
+            response.Message = "Could not retrieve stock data";
+        }
+
+        jsonResponse = JsonConvert.SerializeObject(response);
+        if (response.Status == "success") {
+            return Ok(jsonResponse);
+        }
+        else {
+            return BadRequest(jsonResponse);
+        }
+    }
+
+    [HttpGet("News/{ticker}")]
+    [Authorize]
+    public async Task<IActionResult> GetNewsByTicker(string ticker)
+    {
+        var apiKey = Environment.GetEnvironmentVariable("AlphaVantageAPIKey");
+        var response = new api_response<object> { };
+        var jsonResponse = "";
+        var currentUser = ValidateUser();
+
+        if (currentUser.Id is null) {
+            response.Status = "error";
+            response.Message = "User account not found";
+            jsonResponse = JsonConvert.SerializeObject(response);
+            return NotFound(jsonResponse);
+        }
+
+        var url = $"https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers={ticker}&apikey={apiKey}";
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+        try {
+            var client = _clientFactory.CreateClient("stockData");
+            var data = await client.SendAsync(request);
+            dynamic responseBody = await data.Content.ReadAsStringAsync();
+            JObject result = JsonConvert.DeserializeObject<JObject>(responseBody);
+            response.Data = result;
+            response.Status = "success";
+            response.Message = "Stock data retrieved successfully";
+        }
+        catch (Exception err) {
+            response.Status = "Error";
+            response.Message = "Could not retrieve stock data";
+        }
+
+        jsonResponse = JsonConvert.SerializeObject(response);
+        if (response.Status == "success") {
+            return Ok(jsonResponse);
+        }
+        else {
+            return BadRequest(jsonResponse);
+        }
+    }
+
+    [HttpGet("News/{topic}")]
+    [Authorize]
+    public async Task<IActionResult> GetNewsByTopic(string topic)
+    {
+        var apiKey = Environment.GetEnvironmentVariable("AlphaVantageAPIKey");
+        var response = new api_response<object> { };
+        var jsonResponse = "";
+        var currentUser = ValidateUser();
+
+        if (currentUser.Id is null) {
+            response.Status = "error";
+            response.Message = "User account not found";
+            jsonResponse = JsonConvert.SerializeObject(response);
+            return NotFound(jsonResponse);
+        }
+
+        var url = $"https://www.alphavantage.co/query?function=NEWS_SENTIMENT&topics={topic}&apikey={apiKey}";
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+        try {
+            var client = _clientFactory.CreateClient("stockData");
+            var data = await client.SendAsync(request);
+            dynamic responseBody = await data.Content.ReadAsStringAsync();
+            JObject result = JsonConvert.DeserializeObject<JObject>(responseBody);
+            response.Data = result;
+            response.Status = "success";
+            response.Message = "Stock data retrieved successfully";
+        }
+        catch (Exception) {
             response.Status = "Error";
             response.Message = "Could not retrieve stock data";
         }
